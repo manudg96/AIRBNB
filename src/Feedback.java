ed2dd3466e8456a12d1b86ecf0966e7f761cb0dd
Carmine <carmine.piscitelli11@gmail.com>
2021-06-24 12:53:25 +0200

    feedback 1.2

--- src/Feedback.java
+++ src/Feedback.java
@@ -2,4 +2,45 @@ public class Feedback {
     String IDFeedback;
     String titolo, testo;
     int punteggio;
+
+    public Feedback(String IDFeedback, String titolo, String testo, int punteggio) {
+        this.IDFeedback = IDFeedback;
+        this.titolo = titolo;
+        this.testo = testo;
+        this.punteggio = punteggio;
+    }
+
+    public String getIDFeedback() {
+        return IDFeedback;
+    }
+
+    public void setIDFeedback(String IDFeedback) {
+        this.IDFeedback = IDFeedback;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+
+    public void setTesto(String testo) {
+        this.testo = testo;
+    }
+
+    public int getPunteggio() {
+        return punteggio;
+    }
+
+    public void setPunteggio(int punteggio) {
+        //if (punteggio <= 1 && punteggio => 5){
+            this.punteggio = punteggio;
+       // }
+    }
 }
