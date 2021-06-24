b929aee5193d3234b8b415a5a69b139d13e6795f
Carmine <carmine.piscitelli11@gmail.com>
2021-06-24 15:07:20 +0200

    Aggiunti gli insiemi per DB e metodi vuoti

--- src/AirBNB.java
+++ src/AirBNB.java
@@ -1,2 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class AirBNB {
+    Set<Utente> dbUtenti = new HashSet<>();
+    Set<Abitazione> dbAbitazione = new HashSet<>();
+    Set<Prenotazione> dbPrenotazioni= new HashSet<>();
+    //Set<> dbHost = new HashSet<>();
+
+    void SearchAbitazione (){
+
+    }
+
+    void UltimaPrenotazione (){
+
+    }
+
+    void TopAbitazione (){
+
+    }
+
+    void Top10Host (){
+
+    }
+
+    void AllSuperHost (){
+
+    }
+
+    void Top5Utenti (){
+
+    }
+
+    void MediaPostiLetto (){
+
+    }
+
 }
