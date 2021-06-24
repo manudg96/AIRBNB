b5ea76b3c2bbbcfb8620540c39072c227ea648ff
Carmine <carmine.piscitelli11@gmail.com>
2021-06-24 14:40:41 +0200

    feedback 1.4 aggiunto controllo per punteggio e titolo

--- src/Feedback.java
+++ src/Feedback.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Feedback {
     String IDFeedback;
     String titolo, testo;
     int punteggio;
+
+    Scanner m = new Scanner(System.in);
 
     public Feedback(String IDFeedback, String titolo, String testo, int punteggio) {
         this.IDFeedback = IDFeedback;
@@ -23,7 +27,14 @@ public class Feedback {
     }
 
     public void setTitolo(String titolo) {
-        this.titolo = titolo;
+        if (titolo.length() <= 50 ) {
+            this.titolo = titolo;
+        }else {
+            System.out.println("hai inserito un titolo troppo grande, inserisci massimo 50 caratteri. \n" +
+                    "ne hai inseriti: " + titolo.length());
+            titolo = m.nextLine();
+            setTitolo(titolo);
+        }
     }
 
     public String getTesto() {
@@ -40,8 +51,12 @@ public class Feedback {
 
     public void setPunteggio(int punteggio) {
         //controllo punteggio
-        //if (punteggio <= 1 && punteggio => 5){
+        if ((punteggio >= 1) && (punteggio <= 5)){
             this.punteggio = punteggio;
-       // }
+        }else{
+            System.out.println("Inserisci un punteggio compreso tra 1 e 5");
+            punteggio = m.nextInt();
+            setPunteggio(punteggio);
+        }
     }
 }
