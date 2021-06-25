5a2b9e1253d75ad2032d57ab5438eff04e0b9018
manu <emanuela.deguglielmo@edu.unife.it>
2021-06-25 12:55:54 +0200

    modifica Main
    aggiunta utenti,abitazioni

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        AirBNB airBNB = new AirBNB();
+
+        Utente utente1= new Utente("KG521MN","Mario Draghi", "via Roma 104", "mariodraghi@draghi.it");
+        Utente utente2= new Utente("SKS632D", "Ciccio Banana", "via Mosca 9", "cicciobanana3@mail.com");
+        Utente utente3= new Utente("SLDJ632", "Dante Alighieri", "via Selva Oscura 54","dantealighieri@divinacommedia.it");
+        Utente utente4= new Utente("NDDJFO95","Snoopy Dormiglione", "via Charlie Brown 37", "snoopy@gmail.com");
+
+        Abitazione abitazione1 = new Abitazione("LAKSJDB96","Reggia di Caserta", "via Napoli 91", 281,64,12000);
+        Abitazione abitazione2 = new Abitazione("ALJDDJ98","Baracca senza tetto","via Sotto il Ponte",1,3,0);
+        Abitazione abitazione3 = new Abitazione("LSKDJNDDDO7", "Casa al mare","via Moro 70", 2,6,120);
+        Abitazione abitazione4 = new Abitazione("SKNON87", "Trullo", "via Alberobello 54", 1,4,84);
+        Abitazione abitazione5 = new Abitazione("LAKSJDN89", "Baita","via Cucuzzolo della Montagna",4,16,52);
+
+        //Prenotazione p1 = new Prenotazione("ajhs5",2021-06-05,2021-06-14,utente1,abitazione1);
+
     }
 
 }
