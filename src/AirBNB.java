e8f03620bd80a57e44172544dbf57fa1aa2d3a40
Carmine-P <carmine.piscitelli11@gmail.com>
2021-06-25 12:57:26 +0200

    tutto a rotoli sta andando

--- src/AirBNB.java
+++ src/AirBNB.java
@@ -1,6 +1,12 @@
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.*;
 
 public class AirBNB {
+
+    LocalDate dataOggi = LocalDate.now();
+    LocalDate mesePre = dataOggi.minusMonths(1);
+
 
     public AirBNB() {
         //this.dbUtenti = new HashSet<>();
@@ -20,7 +26,6 @@ public class AirBNB {
     Map<String, Host> dbHost;
     //Set<Host> dbHost;
     Set<Host> superHost;
-
 
     //aggiunge gli host ai super host
     public void updateSuperHost () {
@@ -49,14 +54,31 @@ public class AirBNB {
 
     //ottenere l'abitazione più gettonata nell'ultimo mese
     String TopAbitazione (){
-        List<Abitazione> newDbAbitazione = new ArrayList<>(dbAbitazione);
+        List<Abitazione> newDbAbitazione = new ArrayList<>();
+
+        for (Prenotazione p : dbPrenotazioni.values()) {
+            if (p.getStartDate().isAfter(mesePre) && p.getStartDate().isBefore(dataOggi)){
+                newDbAbitazione.add(p.getAbitazione());
+            }
+        }
         newDbAbitazione.sort(Comparator.comparing(Abitazione::getContPrenotazioni));
         return newDbAbitazione.toString(); // ? da testare
-    }
+    }//end TopAbitazione
+
 
     //ottenere gli host con più prenotazioni nell'ultimo mese
     void Top10Host (){
-
+        for (Host host:dbHost.values()) { //prendo un host
+            for (Prenotazione prenotazione : dbPrenotazioni.values()) { //prendo l abitazione
+                    for (Abitazione abitazione : host.abitazioni) { //controllo l abitazione nell arraylist di host
+                        if (prenotazione.getStartDate().isAfter(mesePre) && prenotazione.getStartDate().isBefore(dataOggi)) {
+                            if (host.abitazioni.contains(abitazione)) {
+                                System.out.println(host);
+                            }
+                        }
+                    }
+                }
+        }
     }
 
     //ottenere tutti i super-host
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Main {
     public static void main(String[] args) {
 
